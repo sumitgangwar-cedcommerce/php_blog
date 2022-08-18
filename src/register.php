e9e29a6a1e155af8f931019584b8db36977d4fc3
sumitgangwar-cedcommerce <104122327+sumitgangwar-cedcommerce@users.noreply.github.com>
2022-08-18 12:48:40 +0530

    Update register.php

--- src/register.php
+++ src/register.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,15 +16,15 @@
   <center>
     <p id="heading-before-register-table">We are <span class="wave" >&#128515;</span> you are joining us.</p>
     <div class="user-registration m-auto">
-        <form method="POST" action="controller.php" enctype="multipart/form-data" onsubmit="validateForm();">
-        <table class="registration-table">
+        <form  method="POST" action="controller.php" enctype="multipart/form-data" onsubmit="validateForm();">
+        <table class="registration-table" style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;">
             
-            <tr><td class="first-td-table">Name</td><td class="second-td-table"><input type="text" name="user" id="name" class="form-input" required></td></tr>
-            <tr><td class="first-td-table">Email</td><td class="second-td-table"><input type="email" name="email" id="email" class="form-input" required></td></tr>
-            <tr><td class="first-td-table">Contact</td><td class="second-td-table"><input type="number" name="contact" id="contact" class="form-input" required></td></tr>
-            <tr><td class="first-td-table">Tag line</td><td class="second-td-table"><input type="text" name="tagline" class="form-input" required></td></tr>
+            <tr><td class="first-td-table">Name</td><td class="second-td-table"><input placeholder="Your Name" type="text" name="user" id="name" class="form-input" required></td></tr>
+            <tr><td class="first-td-table">Email</td><td class="second-td-table"><input placeholder="Email address" type="email" name="email" id="email" class="form-input" required></td></tr>
+            <tr><td class="first-td-table">Contact</td><td class="second-td-table"><input placeholder="### ### ####" type="number" name="contact" id="contact" class="form-input" required></td></tr>
+            <tr><td class="first-td-table">Tag line</td><td class="second-td-table"><input placeholder="Eg:- Delivering technology" type="text" name="tagline" class="form-input" required></td></tr>
             <tr><td class="first-td-table">Upload Image</td><td class="second-td-table"><input type="file" name="img_upload" class="form-input"></td></tr>
-            <tr><td class="first-td-table">Password</td><td class="second-td-table"><input type="password" name="password" id="password" class="form-input" required></td></tr>
+            <tr><div style="color:red;"><?php echo $_SESSION['err3'] ?></div><td class="first-td-table">Password</td><td class="second-td-table"><input placeholder="Eg:- AA12ad@c" type="password" name="password" id="password" class="form-input" required></td></tr>
             <tr><td class="first-td-table">Confirm Password</td><td class="second-td-table"><input type="password" name="confirm_password" id="confirm_password"class="form-input" required></td></tr>
         </table>
         <input type="submit" name="submit_register" class="btn" value="Click to Register">
